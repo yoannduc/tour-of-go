b9391a243f8917c33815af03f98837f5591aa5ca
Yoann <dev.yoannduc@gmail.com>
2019-07-28 20:56:03 +0200

    Implement excercise 2

---
+++ 02-slices.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+  // Define 2 dimension slice
+	var slice [][]uint8
+
+  // For dx param, populate dimension 1 slice with uin8 slice
+	for i := 0 ; i <= dx ; i++ {
+    // Define the dimension 2 slice
+		var sliceInside []uint8
+
+    // For dy param, populate dimension 2 slice with uint8 value
+		for j := 0 ; j <= dy ; j++ {
+      // Append a uint8 value to the dimension 2 slice
+			sliceInside = append(sliceInside, uint8((i + j) / 2))
+		}
+
+    // Append the builded dimension 2 slice to the dimension 1 slice
+		slice = append(slice, sliceInside)
+	}
+
+  // Return the builded 2 dimension slice
+	return slice
+}
+
+func main() {
+	pic.Show(Pic)
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A TOUR OF GO
 
 My take on tour of go website excercises
+
+For excercise 2, you need to run `go get golang.org/x/tour/gotour`
