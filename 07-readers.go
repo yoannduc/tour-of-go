f43cde6a1594ab5954f49e7eb745fa0ff5cdcd9f
Yoann <dev.yoannduc@gmail.com>
2019-07-29 00:54:23 +0200

    Implement excercise 07

---
+++ 07-readers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "golang.org/x/tour/reader"
+  // "io"
+  // "fmt"
+  // "errors"
+)
+
+type MyReader struct{}
+
+func (m *MyReader) Read(b []byte) (int, error) {
+  // For each element in b slice, replace it by byte 'A'
+  for i := range b {
+    // Make use of rune with single quote AKA string litteral
+    b[i] = 'A'
+  }
+
+  // Return len of b with nil error
+  // This is standard reader implementation
+  return len(b), nil
+}
+
+func main() {
+	reader.Validate(&MyReader{})
+}
